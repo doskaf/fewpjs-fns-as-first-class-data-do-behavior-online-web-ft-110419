93bce0011d4617b05bb803a0a1941287a934465e
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 16:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ function greet(string) {
   let time = parseInt(string);
   let hour = string.split(':')[0];
   if (hour > 0 && hour < 12)
+<<<<<<< HEAD
     return "Good Morning";
+=======
+    return "Good Morning.";
+>>>>>>> 397ea82b2c9564ce42348019151a763e2a0a9d0e
   else if (hour > 11 && hour < 18)
     return "Good Afternoon";
   else
     return "Good Evening";
 }
 
+<<<<<<< HEAD
 function displayMessage(greeting) {
   document.getElementById("greeting").innerText = greeting;
 }
+=======
+/* Write your implementation of displayMessage() */
+>>>>>>> 397ea82b2c9564ce42348019151a763e2a0a9d0e
